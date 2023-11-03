1e825ce60aecd9b2990bbebe445d288704afafca
INMD1 <lyw514549@gmail.com>
2023-11-03 18:22:24 +0900

    기본적인 코드생성

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Deu_wear_app"
+include(":app")
+ 
