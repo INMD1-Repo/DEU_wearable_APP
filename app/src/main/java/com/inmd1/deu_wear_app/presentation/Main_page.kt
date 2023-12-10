fb4b9f50728929416904fc80bbc5439186deb44e
INMD1 <87979171+INMD1@users.noreply.github.com>
2023-12-10 21:41:23 +0900

    test

--- app/src/main/java/com/inmd1/deu_wear_app/presentation/Main_page.kt
+++ app/src/main/java/com/inmd1/deu_wear_app/presentation/Main_page.kt
@@ -10,50 +10,90 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.TimeText
+import androidx.wear.compose.navigation.SwipeDismissableNavHost
+import androidx.wear.compose.navigation.composable
 import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
+import com.inmd1.deu_wear_app.presentation.UI.StartPage
 import com.inmd1.deu_wear_app.presentation.theme.Deu_wear_appTheme
 
 class MainActivity : ComponentActivity() {
-    lateinit var navHostController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WearApp()
+            Deu_wear_appTheme() {
+                var navController = rememberSwipeDismissableNavController()
+                SwipeDismissableNavHost(navController = navController, startDestination = "main_page"){
+                    composable("main_page"){
+                        WearApp(navController = navController )
+                    }
+
+                    composable("start_page"){
+                        StartPage(navController = navController)
+                    }
+                }
+            }
         }
     }
 }
 
 @Composable
 //여기에 수정하면 될듯
-fun WearApp() {
-    Deu_wear_appTheme {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            verticalArrangement = Arrangement.Center
-        ) {
-            TimeText()
+fun WearApp(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column(modifier = Modifier.padding(25.dp)) {
             Text(text = "여기는 메인 페이지입니다.")
-            
+        }
+        Button(onClick = { navController.navigate("start_page") }, modifier = Modifier
+            .align(Alignment.CenterHorizontally)
+            .height(50.dp)) {
+            Text(text = "여기로 얌마 화감")
         }
     }
+
+}
+@Composable
+fun testdesing() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column(modifier = Modifier.padding(25.dp)) {
+            Text(text = "여기는 메인 페이지입니다.")
+        }
+        Button(onClick = { /*TODO*/ },  modifier = Modifier
+            .align(Alignment.CenterHorizontally)
+            .height(40.dp)) {
+            Text(text = "얏바래")
+        }
+    }
+
 }
 
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    WearApp()
+    testdesing()
 }
--- app/src/main/java/com/inmd1/deu_wear_app/presentation/UI/startPage.kt
+++ app/src/main/java/com/inmd1/deu_wear_app/presentation/UI/startPage.kt
@@ -6,64 +6,42 @@
 
 package com.inmd1.deu_wear_app.presentation.UI
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
-import com.inmd1.deu_wear_app.R
-import com.inmd1.deu_wear_app.presentation.UI.theme.Deu_wear_appTheme
-
-class mainview : ComponentActivity(
-) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            WearApp()
-        }
-    }
-}
+import com.inmd1.deu_wear_app.presentation.testdesing
 
 @Composable
-fun WearApp() {
-    Deu_wear_appTheme {
-        /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
-         * version of LazyColumn for wear devices with some added features. For more information,
-         * see d.android.com/wear/compose.
-         */
+fun StartPage(navController: NavController) {
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
+            modifier = Modifier.fillMaxSize().background(MaterialTheme.colors.background),
             verticalArrangement = Arrangement.Center
-        ) {
-            Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "동의대 워치")
+        ){
+            Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "동의대 워치", color = Color.White)
             Column(modifier = Modifier.padding(20.dp)) {
-                Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "환영합니다! 동의대 워치는 워치는 사용중인 동의대 학생들의 편의를 돕고자 개발한 앱입니다.",fontSize = 10.sp)
+                Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "환영합니다! 동의대 워치는 워치는 사용중인 동의대 학생들의 편의를 돕고자 개발한 앱입니다.",fontSize = 10.sp, color = Color.White)
             }
             Spacer(modifier = Modifier.height(3.dp))
             Button(
-                { /*TODO*/ },
+                onClick = { navController.navigate("main_page") },
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
                     .width(80.dp)
@@ -72,21 +50,31 @@ fun WearApp() {
                 Text(text = "다음",fontSize = 10.sp)
             }
         }
+}
+@Composable
+fun test() {
+    Column(
+        modifier = Modifier.fillMaxSize().background(MaterialTheme.colors.background),
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "동의대 워치", color = Color.White )
+        Column(modifier = Modifier.padding(20.dp)) {
+            Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "환영합니다! 동의대 워치는 워치는 사용중인 동의대 학생들의 편의를 돕고자 개발한 앱입니다.",fontSize = 10.sp, color = Color.White)
+        }
+        Spacer(modifier = Modifier.height(3.dp))
+        Button(
+            onClick = {  },
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .width(80.dp)
+                .height(30.dp)
+        ) {
+            Text(text = "다음",fontSize = 10.sp)
+        }
     }
 }
-
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
-}
-
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
 @Composable
-fun DefaultPreview2() {
-    WearApp()
+fun DefaultPreview() {
+    test()
 }
